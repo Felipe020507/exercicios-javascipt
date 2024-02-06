7319e54542b12b1a688ea8c09f8be1e693933c1c
Felipe020507 <129906646+Felipe020507@users.noreply.github.com>
2024-02-06 15:47:26 -0300

    Delete Exercicios Math, String, Date directory

--- Exercicios Math, String, Date/Exercicio 1/_js/Principal.js
+++
@@ -1,4 +0,0 @@
-import Cliente from "./Cliente.js"
-
-let cliente1 = new Cliente('Robertinho da Marola', '1962-05-12');
-cliente1.saudacaoPersonalizada();
--- Exercicios Math, String, Date/Exercicio 2/_js/Principal.js
+++
@@ -1,4 +0,0 @@
-import Produto from "./Produto.js";
-
-let produto1 = new Produto('Pedra voce sabe qual', 34.5, '2023-09-11', '2024-06-13');
-produto1.exibirInformacoes();
