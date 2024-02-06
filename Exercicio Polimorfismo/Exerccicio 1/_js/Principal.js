0fd7bf33808d680babe8d5834daa75a9f92892b0
Felipe020507 <129906646+Felipe020507@users.noreply.github.com>
2024-02-06 14:59:00 -0300

    Delete Exercicio Polimorfismo/Exerccicio 1 directory

--- Exercicio Polimorfismo/Exerccicio 1/_js/Principal.js
+++
@@ -1,8 +0,0 @@
-import Professor from "./Professor.js";
-import Aluno from "./Aluno.js";
-
-let prof = new Professor("Marcinho", 35, "Salvar crianças do Jorge");
-let alun = new Aluno("Claudinho", 18, "45689");
-
-console.log(alun.apresentar());
-console.log(prof.apresentar());
